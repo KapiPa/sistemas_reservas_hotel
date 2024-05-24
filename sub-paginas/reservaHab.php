703a4a8ee2c28f0e3d863b3999c99964e7f7df7f
KapiPa <pablodeonce@gmail.com>
2024-05-24 18:52:11 -0300

    solo avanzado sub-paginas

---
+++ sub-paginas/reservaHab.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilos/indexcss.css">
+</head>
+<body>
+    <header>
+        <!-- logo y nombre, menu para ir al historial y las habitaciones diponibles -->
+    </header>    
+    <main>
+        <!-- formulario con nombre, apellido, dni, habitacion, fecha y metodo de pago -->
+        <h2>Reserva una habitacion</h2>
+        <form action="">
+            <label for="Nombre" id="Nombre">Nombre:</label>
+            <input type="text" name="inpNombre" id="inpNombre"><br>
+            <label for="Apellido" id="Apellido">Apellido:</label>
+            <input type="text" name="inpApellido" id="inpApellido"><br>
+            <label for="dni" id="dni">DNI:</label>
+            <input type="number" name="inpDNI" id="inpDNI"><br>
+            <label for="Email" id="Email">Email:</label>
+            <input type="email" name="inpEmail" name="inpEmail">
+            <label for="Habitacion" id="Habitacion">Numero de Habitacion:</label>
+            <input type="number" name="inpHabitacion" id="inpHabitacion"><br>
+            <label for="FechaIngreso" id="FechaIngreso">Fecha de Check-In:</label>
+            <input type="date" name="inpFechaIngreso" id="inpFechaIngreso"><br>
+            <label for="FechaSalida" id="FechaSalida">Fecha de Check-Out:</label>
+            <input type="date" name="inpFechaSalida" id="inpFechaSalida"><br>
+            <input type="submit" value="Enviar">
+        </form>
+    </main>
+    <aside>
+        <!-- ni idea -->
+    </aside>
+    <footer>
+        <!-- info de contacto -->
+    </footer>
+</body>
+</html>
